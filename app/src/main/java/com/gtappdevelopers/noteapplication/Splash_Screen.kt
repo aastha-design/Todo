d4673a5fefd264769ba73a6d07f484eaba253f9c
anupam92402 <anupamg2001@gmail.com>
2021-11-10 12:45:46 +0530

    pushing complete project

---
+++ app/src/main/java/com/gtappdevelopers/noteapplication/Splash_Screen.kt
@@ -0,0 +1,29 @@
+package com.gtappdevelopers.noteapplication
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.os.Handler
+import android.view.WindowManager
+
+class Splash_Screen : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_splash_screen)
+
+        window.setFlags(
+            WindowManager.LayoutParams.FLAG_FULLSCREEN,
+            WindowManager.LayoutParams.FLAG_FULLSCREEN
+        )
+
+        supportActionBar?.hide()
+
+        // we used the postDelayed(Runnable, time) method
+        // to send a message with a delayed time.
+        Handler().postDelayed({
+            val intent = Intent(this, MainActivity::class.java)
+            startActivity(intent)
+            finish()
+        }, 3000)
+    }
+}
